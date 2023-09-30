87cd738e4fdb92247a7c32f9eea3c08f66eeeb74
VaibhavChawla151003 <106020801+VaibhavChawla151003@users.noreply.github.com>
2023-10-01 00:09:39 +0530

    Update server.js

--- server/server.js
+++ server/server.js
@@ -23,10 +23,10 @@ app.use(cors())
 app.get('/', (req, res) => {
   res.json('server is Ok');
 });
-app.use('/api/v1/users',require('./routes/userRoute'));
+app.use('/api/users',require('./routes/userRoute'));
 
 //tasks routes
-app.use('/api/v1/tasks',require('./routes/taskRoutes'));
+app.use('/api/tasks',require('./routes/taskRoutes'));
 
 
 
